e9336bd7faf5c8e0cf9fff9addd1e9a0c3a1ab34
Robert Fenton Merrill <rfmerrill@berkeley.edu>
2013-02-24 12:01:10 -0800

    Let's make a subfolder here...

--- go.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rm krackrainbow.elf krackrainbow.hex
-avr-gcc -DSLAVE_ADDRESS=$1 -O2 -mmcu=atmega328p krackrainbow.c data.c -o krackrainbow.elf
-avr-objcopy -j .text -j .data -O ihex krackrainbow.elf krackrainbow.hex
-avrdude -c avrisp -F -p atmega328p -P /dev/ttyACM0 -b 19200 -Uflash:w:krackrainbow.hex:i
