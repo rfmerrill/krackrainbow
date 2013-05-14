4ff12446584c57495c3e611e3b35bbf34f2d9269
R.F.Merrill <rfmerrill@berkeley.edu>
2013-05-14 03:22:38 +0000

    This doesn't kill performance that much

--- picontrol/krackoutput.py
+++ picontrol/krackoutput.py
@@ -19,6 +19,9 @@ nonhex = re.compile("[^0-9a-fA-F]")
 
 for theline in sys.stdin:
   theline = nonhex.sub("", theline) 
+
+  if ((len(theline) % 2) == 1):
+    theline = theline[:-1]
  
   (address, packet) = (theline[0:2], theline[2:]) 
 
