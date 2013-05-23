31892f6a827cfa981bc864d89a219a03c2d9fe29
R.F.Merrill <rfmerrill@berkeley.edu>
2013-05-23 22:43:14 +0000

    Updates...

---
+++ web/htdocs/sourceindex.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+ addButton("Diamonds", "#8888ee", "/source/diamonds")
+ addButton("SUPER CGOL", "#AAAACC", "/source/cgolmega")
+ addButton("Trippy CGOL", "#8888ee", "/source/cgol2")
+ addButton("PixelController", "#AAAACC", "/source/pixelcontroller")
+}
+
